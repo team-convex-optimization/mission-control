a5ab8b702db8376aac458f15a38e8cf99cf38cab
Edward2k <edulira@me.com>
2022-03-09 17:53:51 +0100

    update scripts to include date in log message

--- scripts/fundamentals.sh
+++ scripts/fundamentals.sh
@@ -4,8 +4,10 @@ bash ~/mission-control/scripts/clean.sh
 
 pushd ~/
     ./tco-actuationd/build/tco_actuationd.bin &
+    echo 1 > /sys/module/ov5645_camera_mipi_v2/parameters/ov5645_af
     ./tco-pland/build/tco_pland.bin -c &
+    echo 1 > /sys/module/ov5645_camera_mipi_v2/parameters/ov5645_af
     ./tco-sensord/build/tco_sensord.bin &
 popd
 
-wall "[Mission Control] Started fundamentals..."
+echo " `date` [Mission Control] Started fundamentals..." >> /home/mendel/log.txt
--- scripts/race.sh
+++ scripts/race.sh
@@ -4,10 +4,12 @@ bash ~/mission-control/scripts/clean.sh
 
 pushd ~/ 
     ./tco-actuationd/build/tco_actuationd.bin &
+    echo 1 > /sys/module/ov5645_camera_mipi_v2/parameters/ov5645_af
     ./tco-pland/build/tco_pland.bin -c &
+    echo 1 > /sys/module/ov5645_camera_mipi_v2/parameters/ov5645_af
     ./tco-pland/build/tco_pland.bin -pr &
     ./tco-sensord/build/tco_sensord.bin &
     ./tco-controld/build/tco_controld.bin & 
 popd
 
-wall "[Mission Control] Started racing pipeline..."
+echo " `date` [Mission Control] Started racing pipeline..." >> /home/mendel/log.txt
